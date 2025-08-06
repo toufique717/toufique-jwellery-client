faf948d0ae12c7334643596fb142e9dd6417c8d5
toufique717 <toufiquehossain716.com>
2025-08-07 03:24:16 +0600

    64.2

---
+++ src/router/Routes.jsx
@@ -0,0 +1,26 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+//import "./index.css";
+
+import Main from "../layout/Main";
+import Home from "../pages/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Main></Main>,
+
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+
+
+  },
+]);
