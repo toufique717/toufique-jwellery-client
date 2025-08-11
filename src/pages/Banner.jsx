f3d6b581d98e03ba066a371075a19bc41c280135
toufique717 <toufiquehossain716.com>
2025-08-11 19:03:38 +0600

    64.3

---
+++ src/pages/Banner.jsx
@@ -0,0 +1,181 @@
+
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import './styles.css';
+
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+import image1 from "../assets/g1.jpg"
+import image2 from "../assets/g2.jpg"
+import image3 from "../assets/g3.jpg"
+import image4 from "../assets/g4.jpg"
+import image5 from "../assets/g5.jpg"
+import image6 from "../assets/g6.jpg"
+import image7 from "../assets/g7.jpg"
+import image8 from "../assets/g8.jpg"
+import image9 from "../assets/g9.jpg"
+
+
+const Banner = () => {
+
+    const progressCircle = useRef(null);
+  const progressContent = useRef(null);
+  const onAutoplayTimeLeft = (s, time, progress) => {
+    progressCircle.current.style.setProperty('--progress', 1 - progress);
+    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
+  };
+    return (
+        <div>
+
+
+
+            <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        onAutoplayTimeLeft={onAutoplayTimeLeft}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+
+         <img
+              src={image1}
+     
+         />
+ 
+        </SwiperSlide>
+
+
+
+        <SwiperSlide>
+
+         <img
+              src={image2}
+     
+         />
+ 
+        </SwiperSlide>
+
+
+
+        <SwiperSlide>
+
+         <img
+              src={image3}
+     
+         />
+ 
+        </SwiperSlide>
+
+
+
+        <SwiperSlide>
+
+         <img
+              src={image4}
+     
+         />
+ 
+        </SwiperSlide>
+
+
+
+
+        <SwiperSlide>
+
+         <img
+              src={image5}
+     
+         />
+ 
+        </SwiperSlide>
+
+
+
+        <SwiperSlide>
+
+         <img
+              src={image6}
+     
+         />
+ 
+        </SwiperSlide>
+
+
+        <SwiperSlide>
+
+         <img
+              src={image7}
+     
+         />
+ 
+        </SwiperSlide>
+
+
+
+
+
+        <SwiperSlide>
+
+         <img
+              src={image8}
+     
+         />
+ 
+        </SwiperSlide>
+
+
+
+
+
+        <SwiperSlide>
+
+         <img
+              src={image9}
+     
+         />
+ 
+        </SwiperSlide>
+
+
+
+
+        <SwiperSlide>
+
+         <img
+              src={image4}
+     
+         />
+ 
+        </SwiperSlide>
+
+
+ 
+        <div className="autoplay-progress" slot="container-end">
+          <svg viewBox="0 0 48 48" ref={progressCircle}>
+            <circle cx="24" cy="24" r="20"></circle>
+          </svg>
+          <span ref={progressContent}></span>
+        </div>
+      </Swiper>
+            
+        </div>
+    );
+};
+
+export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
 
             <h1> This is Home</h1>
+            <Banner></Banner>
             
         </div>
     );
