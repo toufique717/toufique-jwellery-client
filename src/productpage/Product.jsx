23d00f5f8072584d323519bcd7652e910ce8445f
toufique717 <toufiquehossain716.com>
2025-08-18 21:06:03 +0600

    65.2

--- src/productpage/Product.jsx
+++ src/productpage/Product.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+
 import { Helmet } from 'react-helmet-async';
+import Backcover from '../shared/Backcover';
+import Popularitem from '../pages/Popularitem'
+import bimage from '../assets/biimage.jpg'
 
 const Product = () => {
     return (
@@ -7,8 +11,20 @@ const Product = () => {
            <Helmet>
             <title>Jwellery Item</title>
             </Helmet>
-           <h1>This is product page</h1> 
+            
+
+            <Backcover
+            image={bimage}
+            title1={'Our product'}
+            title2={"welcome our shop"}>
+
+            </Backcover>
+
+
+            <Popularitem></Popularitem>
         </div>
+
+        
     );
 };
 
